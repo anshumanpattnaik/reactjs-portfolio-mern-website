7986610a84799802f7b2b3aa5a8bb51d6e0b8884
Anshuman Pattnaik <anshuman.pattnaik524@gmail.com>
2020-06-20 17:45:44 +0300

    Project Dialog implementation

--- src/client/components/ProjectsComponent.js
+++ src/client/components/ProjectsComponent.js
@@ -2,15 +2,40 @@ import React from 'react';
 import "../css/index.css";
 
 import { connect } from 'react-redux';
+import Modal from 'react-modal';
 
 import { fetchProjects } from '../actions';
+
+import githubIcon from '../images/ic_github_white.png';
+import webIcon from '../images/ic_web_site.png';
+
+const modalStyles = {
+    overlay: {
+        backgroundColor: 'rgba(52, 52, 52, 0.8)'
+    },
+    content: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        zIndex: 1000,
+        padding: '0px',
+        borderRadius: '0px',
+        transform: 'translate(-50%, -50%)'
+    }
+};
+
+Modal.setAppElement('#root');
 
 class ProjectsComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             tabItem: ['All', 'Web', 'Mobile'],
-            selectedItem: 'All'
+            selectedItem: 'All',
+            projectItem: null,
+            isDialogOpen: false,
         }
     }
 
@@ -34,7 +59,7 @@ class ProjectsComponent extends React.Component {
                                 <div className={'overlay-item-container'}>
                                     <div className={'overlay-project-div'}>
                                         <p className={'project-title'}>{item.title}</p>
-                                        <div className={'project-readmore-container'}>
+                                        <div className={'project-readmore-container'} onClick={this.onSetProjectDialogState.bind(this, true, item)}>
                                             <span className={'project-readmore-text'}>Read More</span>
                                         </div>
                                     </div>
@@ -44,6 +69,59 @@ class ProjectsComponent extends React.Component {
                         </div>
                     )
                 })
+            )
+        }
+    }
+
+    onSetProjectDialogState = (isShow, item) => {
+        this.setState({
+            isDialogOpen: isShow,
+            projectItem: item
+        })
+    }
+
+    openProjectDialog = () => {
+        var isShow = this.state.isDialogOpen;
+        var item = this.state.projectItem;
+
+        if (isShow) {
+            console.log('Project : ' + isShow + " == " + JSON.stringify(item));
+            return (
+                <Modal
+                    isOpen={isShow}
+                    style={modalStyles}
+                    onRequestClose={this.onSetProjectDialogState.bind(this, false)}
+                    closeTimeoutMS={200}>
+                    <div className={'project-modal-container'}>
+                        <img src={item.thumbnail} className={'project-modal-thumb'} />
+                        <div className={'project-modal-content-container'}>
+                            <span className={'project-modal-title'}>{item.title}</span>
+                            <p className={'project-modal-description'}>{item.description}</p>
+                            <p className={'tech-stack-text'}>Technology Stack</p>
+                            <div className={'project-modal-tech-container'}>
+                                {item.tech_stack.map(tech =>
+                                    <div className={'tech-container-div'}>
+                                        <span className={'tech-span'}>{tech}</span>
+                                    </div>
+                                )}
+                            </div>
+                            <div className={'project-footer-modal'}>
+                                <a href={item.github_link} className={'image-hyperlink'} rel="noopener" target="_blank">
+                                    <div className={'github-site-div'}>
+                                        <img src={githubIcon} className={'visit-site-icon'} />
+                                        <span className={'github-site-text'}>GITHUB</span>
+                                    </div>
+                                </a>
+                                <a href={item.project_link} className={'image-hyperlink'} rel="noopener" target="_blank">
+                                    <div className={'visit-site-div'}>
+                                        <img src={webIcon} className={'visit-site-icon'} />
+                                        <span className={'visit-site-text'}>VISIT SITE</span>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </Modal>
             )
         }
     }
@@ -66,6 +144,7 @@ class ProjectsComponent extends React.Component {
                         {this.renderProjects(results)}
                     </div>
                 </div>
+                {this.openProjectDialog()}
                 <div className={'blank-gap-div'}></div>
             </div>
         );
